ca062f62491aed71a20d6021613ebe631e58c61e
Yajana N Rao <yajananrao@gmail.com>
2019-12-19 15:28:00 +0530

    Create LargestOf3.pl

---
+++ LargestOf3.pl
@@ -0,0 +1,18 @@
+# Find the largest amoung 3 numbers
+print "Enter first number\n";
+$first=<>;
+print "Enter secound number\n";
+$secound=<>;
+print "Enter third number\n";
+$third=<>;
+if($first > $largest){
+  $largest = $first;
+}
+if($secound > $largest){
+  $largest = $secound;
+}
+if($third > $largest){
+  $largest = $third;
+}
+
+print "Largest amoung three is $largest\n";#!/usr/bin/perl
