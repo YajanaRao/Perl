871410082af010161994dc9b26735358f94afdb2
Mahadev Gouda <33411567+MahadevG@users.noreply.github.com>
2020-10-13 20:10:51 +0530

    Create perlArray.pl
    Arry example

---
+++ perlArray.pl
@@ -0,0 +1,15 @@
+# Perl program to demonstrate Array's 
+# creation and accessing the array's elements 
+  
+# Creation an array fruits 
+@fruits = ("apple", "banana", "pineapple", "kiwi"); 
+  
+# printing the array 
+print "@fruits\n"; 
+  
+# Prints the array's elements 
+# one by one using index 
+print "$fruits[0]\n"; 
+print "$fruits[1]\n"; 
+print "$fruits[2]\n"; 
+print "$fruits[3]\n"; 
